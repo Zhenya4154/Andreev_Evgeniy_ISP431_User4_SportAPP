6183e520a55aa7bd56bf88100db563a1141a909e
Коммерческие курсы <comcuru@teach.local>
2024-11-13 12:35:40 +0300

    SportAppMain

---
+++ SportApp/SportApp/Pages/UsersListPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SportApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для UsersListPage.xaml
+    /// </summary>
+    public partial class UsersListPage : Page
+    {
+        public UsersListPage()
+        {
+            InitializeComponent();
+            Init();
+        }
+        private void Init()
+        {
+            try
+            {
+                UsersListView.ItemsSource = Data.User4Entities.GetContext().Users.ToList();
+            }
+            catch (Exception ex) 
+            { 
+
+            }
+        }
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.Manager.MainFrame.Navigate(new Pages.AddEditPage());
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.Manager.MainFrame.Navigate(new Pages.AddEditPage());
+        }
+    }
+}
